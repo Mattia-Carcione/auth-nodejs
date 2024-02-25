438e35246cbbc226cb69f3699a69c3d7fadf95de
Mattia <mattia@MBP-di-Mattia.station>
2024-02-25 21:38:06 +0100

    add login

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/register", async (req, res) => {
     // get the input values
     const email = req.body.emailInput;
     const password = req.body.passwordInput;
-    console.log(email, password);
+
     // check values are there
     if (email && password) {
         try {
@@ -49,6 +49,7 @@ app.post("/register", async (req, res) => {
                 // else register the new user
                 await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
                 // res.render or something else
+                res.sendStatus(200);
             }
         } catch (error) {
             // catch db error
@@ -57,6 +58,50 @@ app.post("/register", async (req, res) => {
         }
     } else {
         // if input values are empty send error status
+        res.status(417).send("Expectation Failed");
+    }
+});
+
+// login page
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
+
+// login
+app.post("/login", async (req, res) => {
+    // get the input values
+    const email = req.body.emailInput;
+    const password = req.body.passwordInput;
+
+    // check input aren't empty
+    if (email && password) {
+        try {
+            // check email exists
+            const emailCheck = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+            if (emailCheck.rows.length > 0) {
+                // save user's credentials
+                const user = emailCheck.rows[0];
+
+                // check the password matches with the input value
+                if (user.password === password) {
+                    // res.render or something else
+                    res.sendStatus(200);
+                } else {
+                    // if password doesn't match send error msg
+                    res.send("Incorrect password.");
+                }
+            } else {
+                // if the email doesn't exists send error msg
+                res.send("User not found.");
+            }
+        } catch (error) {
+            // catch error db and send error msg
+            console.error(error);
+            res.sendStatus(500);
+        }
+    } else {
+        // if the input values are empty send error msg
         res.status(417).send("Expectation Failed");
     }
 });
