59ac8ab06f711d079d0ea1712b4493791afb7022
Mattia <mattia@MBP-di-Mattia.station>
2024-02-25 20:42:57 +0100

    add front end

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // END SETUP
 
+// ROUTES
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+// END ROUTES
+
 // CONFIG SERVER
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
